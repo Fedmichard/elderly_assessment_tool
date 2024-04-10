cc198213e090d281c21d937422dbda11503aa2b0
Victor Morales Blanco <93718568+VictorMoralesBlanco123@users.noreply.github.com>
2024-04-10 18:00:13 -0400

    Updated frontend dependencies
    react-bootstrap was not in the json file

--- Frontend/src/components/SignIn.js
+++ Frontend/src/components/SignIn.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Alert from 'react-bootstrap/Alert';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/bootstrap-5.2.3-dist/PhishEld.css';
+import Alert from 'react-bootstrap/Alert';
 
 
 // Add alert message when email or password is wrong***
